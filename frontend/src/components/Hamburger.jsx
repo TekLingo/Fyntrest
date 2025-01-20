ba21933cdf3ff16b4fba830ee45fdd5b38936dc3
Mohit Sapat <103207988+sapatmohit@users.noreply.github.com>
2025-01-21 00:37:33 +0530

    hamburger menu fixed, menu btn issue presists

--- frontend/src/components/Hamburger.jsx
+++ frontend/src/components/Hamburger.jsx
@@ -1,30 +1,18 @@
-import React, { useState } from "react";
-import Menu from "../assets/Images/Navbar.svg"
+import React from 'react';
 
 const Hamburger = () => {
-  const [nav, setNav] = useState(false);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-  return (
-    <div className="absolute top-0 left-0 w-full h-80">
-      <div className="relative">
-        {/* <svg
-          className="absolute w-full"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
-          <path
-            fill="#362856"
-            d="M0,160L48,170.7C96,181,192,203,288,192C384,181,480,139,576,133.3C672,128,768,160,864,181.3C960,203,1056,213,1152,186.7C1248,160,1344,96,1392,64L1440,32L1440,0L0,0Z"
-          ></path>
-        </svg> */}
-        <div className="bg-Menu h-screen w-full bg-no-repeat bg-contain"></div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="absolute -top-24 left-0 w-full h-96 bg-Menu bg-no-repeat bg-cover z-10 flex justify-center items-center transition-transform transform ease-in-out duration-500">
+			{/* Menu Items */}
+			<ul className="text-white text-xl flex gap-40">
+				<li className="py-4 hover:underline cursor-pointer">Home</li>
+				<li className="py-4 hover:underline cursor-pointer">Courses</li>
+				<li className="py-4 hover:underline cursor-pointer">Blogs</li>
+				<li className="py-4 hover:underline cursor-pointer">About</li>
+				<li className="py-4 hover:underline cursor-pointer">Contact</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Hamburger;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,44 +1,48 @@
-import React, { useState } from "react";
-import logo from "../assets/Images/Color Logo.png";
-import Hamburger from "./Hamburger";
-import { IoIosClose, IoIosMenu } from "react-icons/io";
+import React, { useState } from 'react';
+import { IoIosClose, IoIosMenu } from 'react-icons/io';
+import logo from '../assets/Images/Color Logo.png';
+import Hamburger from './Hamburger';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+	const [nav, setNav] = useState(false);
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
+	const handleNav = () => {
+		setNav(!nav);
+	};
 
-  return (
-    <>
-      {/* <Hamburger /> */}
-      <div className="bg-transparent flex justify-between p-6 px-16 items-center">
-        <div onClick={handleNav} className="flex items-center">
-          {nav ? (
-            <div>
-              <Hamburger className="z-0" />
-              <IoIosClose
-                size={40}
-                color="#d4cfdb"
-                className="hover:cursor-pointer absolute top-12 z-10"
-              />
-            </div>
-          ) : (
-            <IoIosMenu
-              size={40}
-              color="#d4cfdb"
-              className="hover:cursor-pointer absolute"
-            />
-          )}
-        </div>
-        <img src={logo} className="w-28 h-auto" alt="/" />
-        <button type="button" className="bg-text-g w-28 rounded-full h-8">
-          Get Started
-        </button>
-      </div>
-    </>
-  );
+	return (
+		<div className="bg-transparent flex justify-between items-center py-10 px-16 relative z-50">
+			{/* Hamburger Button */}
+			<div
+				onClick={handleNav}
+				className={`cursor-pointer ${nav ? 'text-white' : 'text-gray-400'}`}
+			>
+				{nav ? (
+					<IoIosClose size={40} className="z-99" />
+				) : (
+					<IoIosMenu size={40} className="z-99" />
+				)}
+			</div>
+
+			{/* Logo */}
+			<img
+				src={logo}
+				className="w-28 h-auto absolute left-1/2 transform -translate-x-1/2"
+				alt="Logo"
+			/>
+
+			{/* Get Started Button */}
+			<button
+				type="button"
+				className="bg-text-g w-28 rounded-full h-8 hidden md:block"
+			>
+				Get Started
+			</button>
+
+			{/* Slide-In Hamburger Menu */}
+			{nav && <Hamburger />}
+		</div>
+	);
 };
 
 export default Navbar;
