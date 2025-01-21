e57076496cdde478bf292ea8e7b1e72e923e8cca
Mohit Sapat <103207988+sapatmohit@users.noreply.github.com>
2025-01-22 01:08:17 +0530

    course redirection fix

--- frontend/src/pages/CourseCarousel.jsx
+++ frontend/src/pages/CourseCarousel.jsx
@@ -1,49 +1,50 @@
-import React from "react";
-import "swiper/css"; // Import Swiper styles
-import "swiper/css/effect-fade"; // Import fade effect style
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from 'react';
+import 'swiper/css'; // Import Swiper styles
+import 'swiper/css/effect-fade'; // Import fade effect style
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import components
-import CourseCard from "../components/Cards/CourseCard";
-import { createSlug } from "../utils/helper";
+import CourseCard from '../components/Cards/CourseCard';
+import { createSlug } from '../utils/helper';
 // Import images
-import BasicImg from "../assets/Images/landing page/course/basicsOfFinLiteracy.png";
-import EarnImg from "../assets/Images/landing page/course/Earn.png";
-import MoneyImg from "../assets/Images/landing page/course/money.png";
-import SaveImg from "../assets/Images/landing page/course/Save.png";
+import { Link } from 'react-router-dom';
+import BasicImg from '../assets/Images/landing page/course/basicsOfFinLiteracy.png';
+import EarnImg from '../assets/Images/landing page/course/Earn.png';
+import MoneyImg from '../assets/Images/landing page/course/money.png';
+import SaveImg from '../assets/Images/landing page/course/Save.png';
 
 const courseData = [
-  { title: "Understanding Money", img: MoneyImg },
-  { title: "Basics of Financial Literacy", img: BasicImg },
-  { title: "Earning Money", img: EarnImg },
-  { title: "Saving Money", img: SaveImg },
+	{ title: 'Understanding Money', img: MoneyImg },
+	{ title: 'Basics of Financial Literacy', img: BasicImg },
+	{ title: 'Earning Money', img: EarnImg },
+	{ title: 'Saving Money', img: SaveImg },
 ].map((course) => ({
-  ...course,
-  link: `/course/${createSlug(course.title)}`,
+	...course,
+	slug: createSlug(course.title), // Ensure slug is being created
 }));
 
 const CourseCarousel = () => {
-  return (
-    <Swiper
-      className="mySwiper"
-      slidesPerView={4}
-      autoplay={{ delay: 3000 }}
-      navigation
-      pagination={{ clickable: true }}
-      effect="fade" // Add fade effect
-      fadeEffect={{ crossFade: true }} // Crossfade between slides
-      spaceBetween={20} // Optional: space between slides
-    >
-      {courseData.map((course, index) => (
-        <SwiperSlide key={index}>
-          <Link to={`/course/${course.slug}`}>
-            {/* Link to dynamic course page */}
-            <CourseCard title={course.title} img={course.img} index={index} />
-          </Link>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+	return (
+		<Swiper
+			className="mySwiper"
+			slidesPerView={4}
+			autoplay={{ delay: 3000 }}
+			navigation
+			pagination={{ clickable: true }}
+			effect="fade" // Add fade effect
+			fadeEffect={{ crossFade: true }} // Crossfade between slides
+			spaceBetween={20} // Optional: space between slides
+		>
+			{courseData.map((course, index) => (
+				<SwiperSlide key={index}>
+					<Link to={`/course/${course.slug}`}>
+						{/* Link to dynamic course page */}
+						<CourseCard title={course.title} img={course.img} index={index} />
+					</Link>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 };
 
 export default CourseCarousel;
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,5 +1,8 @@
-export const createSlug = (title) => title.toLowerCase().replace(/\s+/g, "-");
-
-export default {
-  createSlug,
+export const createSlug = (title) => {
+	// Ensure this returns a valid slug
+	if (!title) {
+		console.error('Title is undefined or empty');
+		return '';
+	}
+	return title.toLowerCase().replace(/\s+/g, '-');
 };
