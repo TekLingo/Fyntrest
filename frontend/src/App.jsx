ccdefe6fa2baf418acb07199467243002388990c
Mohit Sapat <103207988+sapatmohit@users.noreply.github.com>
2025-02-01 00:45:15 +0530

    Routing Fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // Import Router components
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'; // Import Router components
 
-import Journey from "./components/Cards/Journey";
-import LandingPage from "./pages/after-login/LandingPage";
-import CoursePage from "./pages/courses/CoursePage";
-import Needs from "./pages/courses/Needs";
-import VideoPage from "./pages/courses/VideoPage";
-import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+// import Journey from "./components/Cards/Journey";
+import LandingPage from './pages/after-login/LandingPage';
+import CoursePage from './pages/courses/CoursePage';
+// import Needs from "./pages/courses/Needs";
+// import VideoPage from "./pages/courses/VideoPage";
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Needs from './pages/courses/Needs';
 
 export default function App() {
-  return (
-    <Router>
-    	<Routes>
-    		{/* Define routes for different pages */}
-    		<Route path="/" element={<Home />} />
-    		<Route path="/signup" element={<Signup />} />
-    		{/* Dynamic route for course page */}
-    		<Route path="/course/:slug" element={<CoursePage />} />
-    		<Route path="/landing-page" element={<LandingPage />} />
-    	</Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				{/* Define routes for different pages */}
+				<Route path="/" element={<Home />} />
+				<Route path="/signup" element={<Signup />} />
+				{/* Dynamic route for course page */}
+				<Route path="/course/:slug" element={<CoursePage />} />
+				<Route path="/Home" element={<LandingPage />} />
+				<Route path="/course/:slug" element={<CoursePage />} />
+			</Routes>
+		</Router>
+	);
 }
--- frontend/src/components/Cards/ContinueCard.jsx
+++ frontend/src/components/Cards/ContinueCard.jsx
@@ -1,41 +1,47 @@
-import React from "react";
-import GirlImg from "../../assets/Images/landing page/after-login/Girl.png";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import GirlImg from '../../assets/Images/landing page/after-login/Girl.png';
 
 const ContinueCard = () => {
-  const vidStatus = ["Let's Start", "Let's Resume"];
+	const vidStatus = ["Let's Start", "Let's Resume"];
+	const User = 'Sarthak';
+	const Video = [
+		'https://www.youtube.com/embed/86cpfsP0aPs?si=YDyEMqkXEWo2OS8M',
+	];
+	const navigate = useNavigate();
 
-  const User = "Sarthak";
+	const handleNavigate = () => {
+		navigate('/home/course');
+	};
 
-  const Video = [
-    "https://www.youtube.com/embed/86cpfsP0aPs?si=YDyEMqkXEWo2OS8M",
-  ];
-
-  return (
-    <div>
-      <div className=" text-primary-fp text-6xl font-bold font-title h-40">
-        <h1>Welcome {User}</h1>
-      </div>
-      {/* Video Section */}
-      <div className="h-96 flex justify-around items-center">
-        <div className="bg-bg-color w-auto text-text-g flex font-body">
-          <div>
-            <img src={GirlImg} alt="" className="w-auto h-64" />
-          </div>
-          <div className="flex text-3xl pt-8">
-            <p>{vidStatus[1]}</p>
-          </div>
-        </div>
-        <div className="w-1/3 h-60">
-          <iframe
-            className="w-full h-full rounded-2xl"
-            src={Video[0]}
-            title="Course Video Thumbnail"
-            allowFullScreen
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="text-primary-fp text-6xl font-bold font-title h-40">
+				<h1>Welcome {User}</h1>
+			</div>
+			{/* Video Section */}
+			<div className="h-96 flex justify-around items-center">
+				<div className="bg-bg-color w-auto text-text-g flex font-body">
+					<div>
+						<img src={GirlImg} alt="" className="w-auto h-64" />
+					</div>
+					<div className="flex text-3xl pt-8" onClick={handleNavigate}>
+						<p>{vidStatus[1]}</p>
+					</div>
+				</div>
+				<div className="w-1/3 h-60 cursor-pointer">
+					<iframe
+						className="w-full h-full rounded-2xl"
+						src={Video[0]}
+						title="Course Video Thumbnail"
+						allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+						sandbox="allow-scripts allow-same-origin"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ContinueCard;
