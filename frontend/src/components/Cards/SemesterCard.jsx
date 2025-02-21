7cdba6a541930ab0767e1d039bdb8171476d3f9e
Sarthak Wadegaonkar <sarthakssg5@gmail.com>
2025-02-21 11:36:49 +0530

    semester page done

--- frontend/src/components/Cards/SemesterCard.jsx
+++ frontend/src/components/Cards/SemesterCard.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
-import { MdKeyboardArrowRight, MdLock } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import CourseImg from "../../assets/Images/landing page/course/Money.png";
 
-const SemesterCard = ({ content, description, topics }) => {
+const SemesterCard = ({ content, topics, index }) => {
   const [nav, setNav] = useState(false);
+  const variantStyles = [
+    {
+      bg: "bg-gradient-to-br from-yellow-700 to-orange-700",
+      position: "-top-4 -right-6",
+    },
+    { bg: "bg-gradient-to-br from-green-400 to-blue-700", position: "-left-6" },
+    {
+      bg: "bg-gradient-to-br from-blue-200 to-violet-500",
+      position: "-bottom-5 -left-5",
+    },
+    {
+      bg: "bg-gradient-to-br from-purple-600 to-purple-950",
+      position: "-top-2 -left-2",
+    },
+  ];
+  const selectedStyle = variantStyles[index % variantStyles.length];
 
   const handleNav = () => setNav((prev) => !prev);
   return (
     <div
       className={`transition-all duration-300 ease-in-out ${
-        nav ? "h-auto items-start" : "h-20 items-center overflow-hidden"
-      } flex flex-col w-full bg-primary_p rounded-2xl font-body text-xl cursor-pointer`}
+        nav ? "h-auto items-start" : "h-20 items-center overflow-hidden cursor-pointer"
+      } flex flex-col w-full bg-primary_p rounded-2xl font-body text-xl`}
       onClick={handleNav}
     >
       <div className="p-6 w-full flex justify-between items-center">
@@ -21,23 +38,34 @@ const SemesterCard = ({ content, description, topics }) => {
         />
       </div>
       {nav && (
-        <div className="p-6 text-white font-thin">
-          <p className="pb-6 text-lg">{description}</p>
-          <div className="flex flex-wrap gap-6 py-6">
+        <div className="p-6 text-white font-thin w-full">
+          <div className="gap-6 py-6 grid grid-cols-2 w-full">
             {topics.map((topic, index) => (
-              <div key={index} className="w-56">
+              <div key={index} className="w-full">
                 {/* Locked video container */}
-                <div className="relative bg-purple-300 rounded-lg h-32 overflow-hidden">
-                  <div className="absolute inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center text-white">
-                    <MdLock className="text-3xl" />
+                <div className="rounded-lg w-full h-9/10 overflow-hidden ">
+                  <div className="flex flex-col items-center justify-center">
+                    <div className="w-full rounded-lg flex flex-col items-center justify-center shadow-lg relative overflow-hidden bg-primary-b cursor-pointer">
+                      {/* Gradient Blur Background */}
+                      <span
+                        className={`absolute w-36 h-36 rounded-full blur-2xl opacity-80 ${selectedStyle.bg} ${selectedStyle.position}`}
+                      ></span>
+                      <div className="flex flex-col items-center gap-4 z-10">
+                        {/* Image */}
+                        <img
+                          src={CourseImg}
+                          title={topic.title}
+                          className="w-2/5 h-auto object-contain"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </div>
+                    <div className="mb-4 flex flex-col justify-start w-full gap-4">
+                      <p className="text-3xl font-normal">{topic.title}</p>
+                      <p className="text-lg font-normal">{topic.description}</p>
+                    </div>
                   </div>
-                  <img
-                    className="w-full h-full"
-                    src={topic.img}
-                    title={topic.title}
-                  />
                 </div>
-                <p className="text-base mt-2">{topic.title}</p>
               </div>
             ))}
           </div>
--- frontend/src/pages/after-login/Semester.jsx
+++ frontend/src/pages/after-login/Semester.jsx
@@ -2,41 +2,51 @@ import React from "react";
 import SemesterCard from "../../components/Cards/SemesterCard";
 import Navbar2 from "../../components/Navbar2";
 import Footer from "../../components/Footer";
+import ContinueCard from "../../components/Cards/ContinueCard";
+import PracticeTest from "../../components/Cards/PracticeTest";
+import CourseImg from "../../assets/Images/landing page/course/Earn.png";
 
 const Semester = () => {
   const semesterData = [
     {
       content: "Semester 1",
-      description: "Introduction to foundational concepts in Computer Science.",
       topics: [
         {
           title: "Introduction to C Programming",
-          img: "https://via.placeholder.com/150",
+          description:
+            "Introduction to foundational concepts in Computer Science.",
+          img: { CourseImg },
         },
         {
           title: "Mathematics for Computing",
-          img: "https://via.placeholder.com/150",
+          description:
+            "Introduction to foundational concepts in Computer Science.",
+          img: { CourseImg },
         },
         {
           title: "Digital Logic Design",
-          img: "https://via.placeholder.com/150",
+          description:
+            "Introduction to foundational concepts in Computer Science.",
+          img: { CourseImg },
         },
       ],
     },
     {
       content: "Semester 2",
-      description: "Expanding on programming and basic data structures.",
       topics: [
         {
           title: "Data Structures & Algorithms",
+          description: "Expanding on programming and basic data structures.",
           img: "https://via.placeholder.com/150",
         },
         {
           title: "Object-Oriented Programming",
+          description: "Expanding on programming and basic data structures.",
           img: "https://via.placeholder.com/150",
         },
         {
           title: "Computer Organization",
+          description: "Expanding on programming and basic data structures.",
           img: "https://via.placeholder.com/150",
         },
       ],
@@ -44,18 +54,27 @@ const Semester = () => {
   ];
 
   return (
-    <div className="text-text-g flex flex-col gap-10">
+    <div className="text-text-g flex flex-col gap-16">
       <Navbar2 />
-      <div className="flex flex-col gap-4 mx-28">
-        <div></div>
-        {semesterData.map((semester, index) => (
-          <SemesterCard
-            key={index}
-            content={semester.content}
-            description={semester.description}
-            topics={semester.topics}
-          />
-        ))}
+      <div className="flex flex-col gap-12 mx-28">
+        <div>
+          <ContinueCard />
+        </div>
+        <div className="flex flex-col gap-8">
+          <h2 className="font-body text-3xl font-bold">Courses</h2>
+          <div className="flex flex-col gap-4">
+            {semesterData.map((semester, index) => (
+              <SemesterCard
+                key={index}
+                content={semester.content}
+                description={semester.description}
+                topics={semester.topics}
+                index={index}
+              />
+            ))}
+          </div>
+        </div>
+        <PracticeTest />
       </div>
       <Footer />
     </div>
