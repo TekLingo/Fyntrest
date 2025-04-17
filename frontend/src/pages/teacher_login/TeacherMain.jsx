7d6b044b768c92e1b4870f14301a8ca46e7d909a
Mohit Sapat <103207988+sapatmohit@users.noreply.github.com>
2025-04-17 17:08:44 +0530

    bug fixed

--- frontend/src/pages/teacher_login/TeacherMain.jsx
+++ frontend/src/pages/teacher_login/TeacherMain.jsx
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import TeacherNavbar from '../../components/TeacherNavbar';
-import TeacherSidebar from '../../components/TeacherSidebar';
-import Content from './TContent';
-import Dashboard from './TDashboard';
-import Logout from './TLogout';
-import TQuiz from './TQuiz';
-import Settings from './TSettings';
-import Users from './TUsers';
-
-const TeacherPanel = () => {
-	return (
-		<div className="flex h-screen">
-			{/* Ensure full height */}
-			<TeacherSidebar /> {/* Sidebar with links */}
-			<div className="w-full flex flex-col">
-				<TeacherNavbar />
-				<div className="flex-grow overflow-auto">
-					{/* Allow scrolling */}
-					<Routes>
-						<Route path="/dashboard" element={<Dashboard />} />
-						<Route path="/users" element={<Users />} />
-						<Route path="/content" element={<Content />} />
-						<Route path="/settings" element={<Settings />} />
-						<Route path="/logout" element={<Logout />} />
-						<Route path="*" element={<Dashboard />} /> {/* Default route */}
-					</Routes>
-				</div>
-			</div>
-		</div>
-	);
-=======
-import React from "react";
+.import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TeacherNavbar from "../../components/TeacherNavbar";
 import TeacherSidebar from "../../components/TeacherSidebar";
@@ -73,7 +39,6 @@ const TeacherPanel = () => {
       </div>
     </div>
   );
->>>>>>> e35bbb489e9a1cb35ff4f7d115f1afcabba7cc8a
 };
 
 export default TeacherPanel;
